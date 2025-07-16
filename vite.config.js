72a1825f08e858bbafc765f41509ff9f9f45a4c5
viglino <jean-marc.viglino@ign.fr>
2025-07-16 16:48:53 +0200

    [BUILD]

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
+
 export default defineConfig(({ command }) => ({
   base: command === 'dev' ? '/' : '/editeur-carto/',
+
+  build: {
+    outDir: './docs',
+    emptyOutDir: true, // also necessary
+  },
 }));
+
+
